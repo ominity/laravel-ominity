5a32f18ff27a862c5f49c7c3ebe8a1ed7eed3570
Tim De Smet <tim.desmet@wizzou.com>
2024-04-20 18:52:24 +0200

    Minor laravel pint fixes for ominity component

--- src/Views/Components/OminityComponent.php
+++ src/Views/Components/OminityComponent.php
@@ -18,7 +18,7 @@ abstract class OminityComponent extends Component
     public $fields;
 
     /**
-     * @param array $fields
+     * @param  array  $fields
      */
     public function __construct($fields)
     {
@@ -27,41 +27,44 @@ abstract class OminityComponent extends Component
 
     /**
      * Set current page as global variable for the OminityComponent class
-     * 
-     * @param Page $page
+     *
      * @return void
      */
-    public static function setPage(Page $page) {
+    public static function setPage(Page $page)
+    {
         self::$page = $page;
     }
 
     /**
      * Get field value by it's key
-     * 
-     * @param string $key
+     *
+     * @param  string  $key
      * @return string|null
      */
-    public function field($key) {
+    public function field($key)
+    {
         $value = $this->fields[$key] ?? null;
+
         return $value instanceof Component ? $value->render() : $value;
     }
 
     /**
      * Make field values render friendly / render nested components to HTML
-     * 
-     * @param array $fiedls
+     *
+     * @param  array  $fiedls
      * @return array
      */
     protected function prepareFields($fields)
     {
         foreach ($fields as $key => $field) {
             if (is_array($field) && isset($field['component'])) {
-                $componentClass = config('ominity.components.' . $field['component']);
+                $componentClass = config('ominity.components.'.$field['component']);
                 if ($componentClass && class_exists($componentClass)) {
                     $fields[$key] = new $componentClass($field['fields'] ?? []);
                 }
             }
         }
+
         return $fields;
     }
 }
