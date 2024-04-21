7f5348eeecdfbbda780251d03e5a59b9d51264e5
Tim De Smet <tim.desmet@wizzou.com>
2024-04-21 02:20:28 +0200

    Fixed styling

--- src/Views/Components/OminityComponent.php
+++ src/Views/Components/OminityComponent.php
@@ -45,7 +45,7 @@ abstract class OminityComponent extends Component
     /**
      * Get a field value by key. Handles both single component fields and arrays of components.
      *
-     * @param string $key The field key to retrieve.
+     * @param  string  $key  The field key to retrieve.
      * @return mixed The field value rendered as HTML, or null if not set.
      */
     public function field($key)
@@ -61,12 +61,11 @@ abstract class OminityComponent extends Component
         }
     }
 
-
     /**
      * Prepare fields for rendering, converting nested components as needed.
      * This method now also initializes arrays of components.
      *
-     * @param stdClass $fields Object containing all fields for the component.
+     * @param  stdClass  $fields  Object containing all fields for the component.
      * @return stdClass The modified fields object with nested components initialized.
      */
     protected function prepareFields(stdClass $fields)
@@ -74,7 +73,7 @@ abstract class OminityComponent extends Component
         foreach ($fields as $key => $field) {
             if (is_object($field) && isset($field->component)) {
                 // Initialize a single nested component
-                $componentClass = config('ominity.components.' . $field->component);
+                $componentClass = config('ominity.components.'.$field->component);
                 if ($componentClass && class_exists($componentClass)) {
                     $fields->$key = new $componentClass($field->fields ?? new stdClass());
                 }
@@ -82,15 +81,17 @@ abstract class OminityComponent extends Component
                 // Initialize an array of nested components
                 $fields->$key = array_map(function ($item) {
                     if (is_object($item) && isset($item->component)) {
-                        $componentClass = config('ominity.components.' . $item->component);
+                        $componentClass = config('ominity.components.'.$item->component);
                         if ($componentClass && class_exists($componentClass)) {
                             return new $componentClass($item->fields ?? new stdClass());
                         }
                     }
+
                     return $item;
                 }, $field);
             }
         }
+
         return $fields;
     }
 }
