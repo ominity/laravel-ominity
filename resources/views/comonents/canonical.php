ef27fef4f60944131d7254dd557927d85dd5dba2
Tim De Smet <tim.desmet@wizzou.com>
2024-06-24 11:20:23 +0200

    Bug fix in canonical view

--- resources/views/comonents/canonical.php
+++ resources/views/comonents/canonical.php
@@ -1,7 +1,7 @@
 @foreach($routes as $lang => $route)
     @if($lang == app()->getLocale()
-    <link rel="canonical" href="{{ Ominity::router()->route($route, $lang, $keepQuery) }}">
+    <link rel="canonical" href="{{ \Ominity\Laravel\Facades\Ominity::router()->route($route, $lang, $keepQuery) }}">
     @else
-    <link rel="alternate" href="{{ Ominity::router()->route($route, $lang, $keepQuery) }}" hreflang="{{ $lang }}">
+    <link rel="alternate" href="{{ \Ominity\Laravel\Facades\Ominity::router()->route($route, $lang, $keepQuery) }}" hreflang="{{ $lang }}">
     @endif
 @endforeach
