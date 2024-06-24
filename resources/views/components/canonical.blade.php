d34d3f03749a29042f16bd419bc3b612e2307414
Tim De Smet <tim.desmet@wizzou.com>
2024-06-24 11:33:32 +0200

    Typo in components filter name

--- resources/views/components/canonical.blade.php
+++ resources/views/components/canonical.blade.php
@@ -1,5 +1,5 @@
 @foreach($routes as $lang => $route)
-    @if($lang == app()->getLocale()
+    @if($lang == app()->getLocale())
     <link rel="canonical" href="{{ \Ominity\Laravel\Facades\Ominity::router()->route($route, $lang, $keepQuery) }}">
     @else
     <link rel="alternate" href="{{ \Ominity\Laravel\Facades\Ominity::router()->route($route, $lang, $keepQuery) }}" hreflang="{{ $lang }}">
