337b714cb87f0c55b3c4ae75ea06b5860aac6e47
Tim De Smet <tim.desmet@wizzou.com>
2024-08-28 22:10:40 +0200

    Fixed minor issue

--- resources/views/components/form-field/metadata.blade.php
+++ resources/views/components/form-field/metadata.blade.php
@@ -1,13 +1,13 @@
 @foreach($field->options as $option)
     @if($option == 'page_title')
-        <input type="hidden" name="{{ $field->id }}[{{ $option }}]" value="" id="{{ $id }}_{{ $option }}">
+        <input type="hidden" name="{{ $field->id }}[{{ $option }}]" value="" id="{{ $id }}-{{ $option }}">
         <script>
-            document.getElementById('{{ $field->id }}_{{ $option }}').value = document.title;
+            document.getElementById('{{ $id }}-{{ $option }}').value = document.title;
         </script>
     @elseif($option == 'page_url') 
-        <input type="hidden" name="{{ $field->id }}[{{ $option }}]" value="" id="{{ $id }}_{{ $option }}">
+        <input type="hidden" name="{{ $field->id }}[{{ $option }}]" value="" id="{{ $id }}-{{ $option }}">
         <script>
-            document.getElementById('{{ $field->id }}_{{ $option }}').value = window.location.href;
+            document.getElementById('{{ $id }}-{{ $option }}').value = window.location.href;
         </script>
     @endif
 @endforeach
