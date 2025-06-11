c4b80e6b98be566f4bcc0644c1e98ac24e8ac3be
Tim De Smet <tim.desmet@wizzou.com>
2025-06-11 22:39:11 +0200

    Few changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,34 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    publicDir: false,
     build: {
-        lib: {
-            entry: 'resources/ts/index.ts',
-            name: 'Ominity',
-            fileName: (format) => `ominity.${format}.js`,
-        },
+        sourcemap: true,
+        outDir: 'dist',
+        minify: 'esbuild',
+        emptyOutDir: true,
         rollupOptions: {
+            input: {
+                js: 'resources/ts/index.ts',
+                css: 'resources/scss/ominity.scss',
+            },
             output: {
+                entryFileNames: (chunk) => {
+                    if (chunk.name === 'js') return 'ominity.js';
+                    if (chunk.name === 'css') return 'ominity.css';
+                    return '[name].js';
+                },
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name == 'ominity.scss') {
+                        return 'ominity.css';
+                    }
+                    return '[name].[ext]';
+                },
                 globals: {
                     jquery: '$',
                 },
             },
-            external: ['jquery'], // keep jQuery external
-        },
-        minify: 'esbuild',
+            external: ['jquery'],
+        }
     },
 });
