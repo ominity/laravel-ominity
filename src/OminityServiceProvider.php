197361899d4268e0f50943c4ecb180374091ed0e
Tim De Smet <tim.desmet@wizzou.com>
2025-06-12 02:29:42 +0200

    Updated version

--- src/OminityServiceProvider.php
+++ src/OminityServiceProvider.php
@@ -22,7 +22,7 @@ use Ominity\Laravel\Services\VatValidationService;
 
 class OminityServiceProvider extends ServiceProvider
 {
-    const PACKAGE_VERSION = '1.1.8';
+    const PACKAGE_VERSION = '1.2.0';
 
     /**
      * Boot the service provider.
